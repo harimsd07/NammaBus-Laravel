29fb8fde3e46af6171da1c78ed668a23dcd5c64d
harimsd07 <irah07@tutanota.com>
2026-03-28 21:15:08 +0530

    fix: trust proxies for HTTPS behind Render load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-         $middleware->alias([
+     $middleware->trustProxies(at: '*');
+    $middleware->alias([
         'admin.auth' => \App\Http\Middleware\AdminAuth::class,
     ]);
     })
